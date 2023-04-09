04037c6f619ee0b45a77e95d6290a559abca7778
Paulo Torrealba <paulo@buscalibre.com>
2023-04-09 01:51:02 -0400

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
-				<h1>hola</h1>
+				<h1>hola 0150</h1>
 			</main>
 		</>
 	);
