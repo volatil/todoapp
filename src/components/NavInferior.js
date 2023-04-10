f78332f030ab25103d1d67e24f0f6acb2648f7f6
Paulo <contacto@volatil.cl>
2023-04-10 16:41:26 -0400

    set items left

--- src/components/NavInferior.js
+++ src/components/NavInferior.js
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
+
 import navcss from "../styles/Navcss.module.css";
 import dark from "../styles/Darkmode.module.css";
 
-export default function NavInferior() {
+export default function NavInferior(props) {
+	const { losTODO } = props;
+	
 	return (
 		<nav className={`${navcss.menu} ${dark.menu}`}>
-			<p>5 items left</p>
+			{
+				losTODO
+					? (<p>{losTODO.length} items left</p>)
+					: (<p>0 items left</p>)
+			}
 			<ul>
 				<li>all</li>
 				<li>active</li>
