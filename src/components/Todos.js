aed2e9ca6e56d14a68239f02dc04ef117f49711b
Paulo Torrealba <paulo@buscalibre.com>
2023-04-11 01:51:37 -0400

    lo limpie de comentarios

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -12,31 +12,25 @@ function TodoVacio() {
 
 export default function Todos(props) {
 	const { losTODO, setlosTODO } = props;
-	const [checkTODO, setcheckTODO] = useState(false);
-	console.debug( "hola" );
-	// useEffect(() => {
-	// 	const algo = localStorage.getItem("todoapp_db").length === 2 && Boolean(localStorage.getItem("todoapp_db"));
-	// 	setcheckTODO(algo);
-	// }, []);
 
 	return (
 		<section id="todos" className={`${todos.todos} ${dark.todos}`}>
 			<ul>
-				{ !losTODO && <TodoVacio /> }
-				{/* { checkTODO && <TodoVacio /> } */}
 				{
-					losTODO?.map((todo) => {
-						const { fechacompleta, task } = todo;
-						return (
-							<li key={fechacompleta}>
-								<label htmlFor={fechacompleta}>
-									<input id={fechacompleta} type="checkbox" />
-									{task}
-								</label>
-								<Image onClick={() => setlosTODO( eliminar(fechacompleta) )} className={todos.cerrar} src="/assets/svg/cerrar.svg" width={15} height={15} alt="Cerrar" />
-							</li>
-						);
-					})
+					losTODO
+						? losTODO?.map((todo) => {
+							const { fechacompleta, task } = todo;
+							return (
+								<li key={fechacompleta}>
+									<label htmlFor={fechacompleta}>
+										<input id={fechacompleta} type="checkbox" />
+										{task}
+									</label>
+									<Image onClick={() => setlosTODO( eliminar(fechacompleta) )} className={todos.cerrar} src="/assets/svg/cerrar.svg" width={15} height={15} alt="Cerrar" />
+								</li>
+							);
+						})
+						: <TodoVacio />
 				}
 			</ul>
 		</section>
